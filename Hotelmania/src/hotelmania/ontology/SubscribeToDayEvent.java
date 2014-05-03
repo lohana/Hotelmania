14971b9cae613fe64f98fec91b20a39023a8de7f
Lohana Lema Moreta. <lohana.8@gmail.com>
2014-05-03 21:51:03 +0200

    adding sprint 2 ontology files

---
+++ Hotelmania/src/hotelmania/ontology/SubscribeToDayEvent.java
@@ -0,0 +1,15 @@
+package hotelmania.ontology;
+
+
+import jade.content.*;
+import jade.util.leap.*;
+import jade.core.*;
+
+/**
+* Protege name: SubscribeToDayEvent
+* @author ontology bean generator
+* @version 2014/05/1, 06:37:44
+*/
+public class SubscribeToDayEvent implements AgentAction {
+
+}
